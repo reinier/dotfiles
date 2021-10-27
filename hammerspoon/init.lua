9c64fba8ed8119be5fbb244fff47148d2ba8fd2c
Reinier Ladan <reinierl@gmail.com>
2021-10-27 16:17:41 +0200

    Fixes

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,8 +3,21 @@ require('layerdisplay')
 require('hyperconfig')
 -- require('hammerbrowser')
 
+hs.loadSpoon("SpoonInstall")
+
+spoon.SpoonInstall:andUse('Emojis', {
+	hotkeys = {
+		toggle = { { "alt", "shift", "cmd" }, "e" },
+	}
+})
+
 cherry = hs.loadSpoon("Cherry")
+-- emojis = hs.loadSpoon("Emojis")
 
 cherry:bindHotkeys({
 	start = {{ "alt", "shift", "cmd" }, "p"}
 })
+
+-- emojis:bindHotkeys({
+-- 	toggle = {{ "alt", "shift", "cmd" }, "e"}
+-- })
