273f9036223b974ba382a1e1c17e106b1fcd8ad3
Reinier Ladan <reinierl@gmail.com>
2021-11-05 16:29:42 +0100

    Update threesixbase.c

--- microdox/threesixbase.c
+++ microdox/threesixbase.c
@@ -32,29 +32,164 @@ layer_state_t layer_state_set_user(layer_state_t state) {
   return state;
 }
 
-// COMBOS
+// Tap Dance declarations
+enum {
+    TD_COMM_QUOT,
+    TD_DOT_SCOLN,
+    TD_SLASH_DASH,
+};
 
-// enum combos {
-//   COM_LEFT,
-//   COM_DOWN,
-//   COM_UP,
-//   COM_RIGHT,
-//   COMBO_LENGTH
-// };
-// 
-// uint16_t COMBO_LEN = COMBO_LENGTH;
-// 
-// const uint16_t PROGMEM combo_left[]  = {KC_N, KC_M, COMBO_END};
-// const uint16_t PROGMEM combo_down[]  = {KC_M, KC_COMM, COMBO_END};
-// const uint16_t PROGMEM combo_up[]    = {KC_COMM, KC_DOT, COMBO_END};
-// const uint16_t PROGMEM combo_right[] = {KC_DOT, KC_SLSH, COMBO_END};
-// 
-// combo_t key_combos[] = {
-//     [COM_LEFT]  = COMBO(combo_left, KC_LEFT),
-//     [COM_DOWN]  = COMBO(combo_down, KC_DOWN),
-//     [COM_UP]    = COMBO(combo_up, KC_UP),
-//     [COM_RIGHT] = COMBO(combo_right, KC_RGHT),
-// };
+// - - -
+
+// Define a type containing as many tapdance states as you need
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP
+} td_state_t;
+
+// Create a global instance of the tapdance state type
+static td_state_t td_state;
+
+// Declare your tapdance functions:
+
+// Function to determine the current tapdance state
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+// `finished` and `reset` functions for each tapdance keycode
+void commquot_finished(qk_tap_dance_state_t *state, void *user_data);
+void commquot_reset(qk_tap_dance_state_t *state, void *user_data);
+
+void dotscoln_finished(qk_tap_dance_state_t *state, void *user_data);
+void dotscoln_reset(qk_tap_dance_state_t *state, void *user_data);
+
+void slashdash_finished(qk_tap_dance_state_t *state, void *user_data);
+void slashdash_reset(qk_tap_dance_state_t *state, void *user_data);
+
+// Determine the tapdance state to return
+td_state_t cur_dance(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        else return TD_SINGLE_HOLD;
+    }
+
+    if (state->count == 2) return TD_DOUBLE_SINGLE_TAP;
+    else return TD_UNKNOWN; // Any number higher than the maximum state value you return above
+}
+
+// Handle the possible states for each tapdance keycode you define:
+
+void commquot_finished(qk_tap_dance_state_t *state, void *user_data) {
+    td_state = cur_dance(state);
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            register_code16(KC_COMM);
+            break;
+        case TD_SINGLE_HOLD:
+            tap_code16(KC_QUOT);
+            break;
+        case TD_DOUBLE_SINGLE_TAP:
+            tap_code16(KC_COMM);
+            register_code16(KC_COMM);
+        case TD_NONE:
+        case TD_UNKNOWN:
+            break;
+    }
+}
+
+void commquot_reset(qk_tap_dance_state_t *state, void *user_data) {
+    switch (td_state) {
+        case TD_SINGLE_TAP:
+            unregister_code16(KC_COMM);
+            break;
+        case TD_SINGLE_HOLD:
+            break;
+        case TD_DOUBLE_SINGLE_TAP:
+            unregister_code16(KC_COMM);
+            break;
+        case TD_NONE:
+        case TD_UNKNOWN:
+            break;
+    }
+}
+
+void dotscoln_finished(qk_tap_dance_state_t *state, void *user_data) {
+  td_state = cur_dance(state);
+  switch (td_state) {
+      case TD_SINGLE_TAP:
+          register_code16(KC_DOT);
+          break;
+      case TD_SINGLE_HOLD:
+          tap_code16(KC_SCLN);
+          break;
+      case TD_DOUBLE_SINGLE_TAP:
+          tap_code16(KC_DOT);
+          register_code16(KC_DOT);
+      case TD_NONE:
+      case TD_UNKNOWN:
+          break;
+  }
+}
+
+void dotscoln_reset(qk_tap_dance_state_t *state, void *user_data) {
+  switch (td_state) {
+      case TD_SINGLE_TAP:
+          unregister_code16(KC_DOT);
+          break;
+      case TD_SINGLE_HOLD:
+          break;
+      case TD_DOUBLE_SINGLE_TAP:
+          unregister_code16(KC_DOT);
+          break;
+      case TD_NONE:
+      case TD_UNKNOWN:
+          break;
+  }
+}
+
+void slashdash_finished(qk_tap_dance_state_t *state, void *user_data) {
+  td_state = cur_dance(state);
+  switch (td_state) {
+      case TD_SINGLE_TAP:
+          register_code16(KC_SLASH);
+          break;
+      case TD_SINGLE_HOLD:
+          tap_code16(KC_MINUS);
+          break;
+      case TD_DOUBLE_SINGLE_TAP:
+          tap_code16(KC_SLASH);
+          register_code16(KC_SLASH);
+      case TD_NONE:
+      case TD_UNKNOWN:
+          break;
+  }
+}
+
+void slashdash_reset(qk_tap_dance_state_t *state, void *user_data) {
+  switch (td_state) {
+      case TD_SINGLE_TAP:
+          unregister_code16(KC_SLASH);
+          break;
+      case TD_SINGLE_HOLD:
+          break;
+      case TD_DOUBLE_SINGLE_TAP:
+          unregister_code16(KC_SLASH);
+          break;
+      case TD_NONE:
+      case TD_UNKNOWN:
+          break;
+  }
+}
+
+// - - - Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // [TD_COMM_QUOT] = ACTION_TAP_DANCE_DOUBLE(KC_COMM, KC_QUOT),
+    [TD_COMM_QUOT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, commquot_finished, commquot_reset),
+    [TD_DOT_SCOLN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dotscoln_finished, dotscoln_reset),
+    [TD_SLASH_DASH] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, slashdash_finished, slashdash_reset),
+};
 
 /*
 
@@ -106,9 +241,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 //
 #define KR_0_3_6 KC_N
 #define KR_0_3_7 KC_M
-#define KR_0_3_8 KC_COMM
-#define KR_0_3_9 KC_DOT
-#define KR_0_3_10 KC_SLSH
+#define KR_0_3_8 TD(TD_COMM_QUOT)
+#define KR_0_3_9 TD(TD_DOT_SCOLN)
+#define KR_0_3_10 TD(TD_SLASH_DASH)
 
 // Thumb cluster
 
@@ -125,43 +260,43 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 // ###### Layer 1 Characters
 
 #define KR_1_1_1 KC_ESC
-#define KR_1_1_2 KC_DEL
+#define KR_1_1_2 KC_NO
 #define KR_1_1_3 LALT(KC_2) // €
 #define KR_1_1_4 LSFT(KC_6)
 #define KR_1_1_5 LSFT(KC_BSLASH)
 
-#define KR_1_1_6 KC_AMPR
+#define KR_1_1_6 KC_NO
 #define KR_1_1_7 LSFT(KC_LBRC)
 #define KR_1_1_8 LSFT(KC_RBRC)
-#define KR_1_1_9 KC_UNDS
+#define KR_1_1_9 KC_NO
 #define KR_1_1_10 KC_BSPC
 
 // Mid row
 
-#define KR_1_2_1 LSFT(KC_2) // @
-#define KR_1_2_2 KC_DQUO
-#define KR_1_2_3 KC_QUOT
-#define KR_1_2_4 KC_MINUS
+#define KR_1_2_1 KC_NO
+#define KR_1_2_2 KC_HASH
+#define KR_1_2_3 LSFT(KC_2) // @
+#define KR_1_2_4 KC_EXLM
 #define KR_1_2_5 KC_PPLS
 //
-#define KR_1_2_6 KC_COLN
+#define KR_1_2_6 KC_AMPR
 #define KR_1_2_7 KC_LPRN
 #define KR_1_2_8 KC_RPRN
-#define KR_1_2_9 KC_HASH
-#define KR_1_2_10 KC_EXLM
+#define KR_1_2_9 KC_GRV
+#define KR_1_2_10 KC_NO
 
 // Lower row
 
-#define KR_1_3_1 KC_TILD
+#define KR_1_3_1 KC_NO
 #define KR_1_3_2 KC_BSLASH
-#define KR_1_3_3 KC_GRV
+#define KR_1_3_3 KC_TILD
 #define KR_1_3_4 KC_ASTR
 #define KR_1_3_5 KC_PEQL
 //
-#define KR_1_3_6 KC_SCLN
+#define KR_1_3_6 KC_NO
 #define KR_1_3_7 KC_LBRC
 #define KR_1_3_8 KC_RBRC
-#define KR_1_3_9 LOPT(KC_SCLN)
+#define KR_1_3_9 LOPT(KC_SCLN) // …
 #define KR_1_3_10 TO(3)
 
 
@@ -176,7 +311,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 // ##### Layer 2 Numbers
 
 #define KR_2_1_1 KC_ESC
-#define KR_2_1_2 KC_DEL
+#define KR_2_1_2 KC_NO
 #define KR_2_1_3 LALT(KC_2) // €
 #define KR_2_1_4 KC_PERC
 #define KR_2_1_5 KC_NO
@@ -224,7 +359,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 // ##### Layer 3 Navigation
 
 #define KR_3_1_1 KC_ESC
-#define KR_3_1_2 KC_DEL
+#define KR_3_1_2 KC_NO
 #define KR_3_1_3 KC_NO
 #define KR_3_1_4 KC_NO
 #define KR_3_1_5 KC_NO
