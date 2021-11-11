fb0d87efb3fea30b9150debe2dc2de6edff0670e
Reinier Ladan <reinierl@gmail.com>
2021-11-11 11:01:42 +0100

    Removed hammerspoon appswitcher

--- hammerspoon/hypers.lua
+++ hammerspoon/hypers.lua
@@ -1,9 +1,9 @@
 -- Reserved:
 
 -- A, J, K, L - Keyboard Maestro
--- C - Alfred
--- T, M - Alfred
+-- C, T - Alfred
 -- H - Hyperhammer
+-- M - MenuAnywhere
 
 hs.hotkey.bind(hyperkey, "d", function()
 	hs.application.launchOrFocusByBundleID('com.agiletortoise.Drafts-OSX')
@@ -32,14 +32,3 @@ end)
 hs.hotkey.bind(hyperkey, "e", function()
 	hs.application.launchOrFocusByBundleID('com.panic.nova')
 end)
-
--- Application switcher
-hs.window.switcher.ui.fontName = 'Menlo'
-hs.window.switcher.ui.textSize = 9 -- in screen points
-hs.window.switcher.ui.showTitles = true -- show window titles
-hs.window.switcher.ui.showThumbnails = false -- show window thumbnails
-hs.window.switcher.ui.showSelectedThumbnail = false -- show a larger thumbnail for the currently selected window
-switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{})
-
-hs.hotkey.bind(hyperkey,'q', function()switcher:next()end)
-hs.hotkey.bind(hyperkey,'w', function()switcher:previous()end)
