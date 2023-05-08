efa941a9a6e195a0cb2984ce651ef373018669c0
Reinier Ladan <reinier@ladan.email>
2023-05-08 09:29:12 +0200

    Update keymap.c

--- keychron-q9/keymap.c
+++ keychron-q9/keymap.c
@@ -44,26 +44,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 }
 
 /* Signal a layer switch with Programmable Keys */
-#include "features/layer_signal.c"
+/* #include "features/layer_signal.c" */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_52(
         HYPR_T(KC_TAB),  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,          KC_MUTE,
         MEH_T(KC_ESC), KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,             REPEAT,
         SC_LSPO,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            SC_RSPC, KC_UP,
-        OSM(MOD_LCTL), OSM(MOD_LALT),  OSM(MOD_LGUI),              LT(_FN3,KC_SPC),                      OSM(MOD_RGUI),  TO(_FN1), TO(_FN3), KC_LEFT, KC_DOWN, KC_RGHT),
+        OSM(MOD_LCTL), OSM(MOD_LALT),  OSM(MOD_LGUI),              LT(_FN3,KC_SPC),                      OSM(MOD_RGUI),  MO(_FN3), MO(_FN1), KC_LEFT, KC_DOWN, KC_RGHT),
 
     [_FN1] = LAYOUT_ansi_52(
         KC_GRV,  KC_BRID,  KC_BRIU,  KC_NO,   KC_NO,   RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_DEL,           _______,
         RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______,  _______,            _______,          _______,
         _______,           RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, NK_TOGG, _______, _______, _______,  _______,            _______, _______,
-        _______, _______,  _______,                             _______,                            _______,  TO(MAC_BASE),  TO(_FN3),  _______, _______, _______),
+        _______, _______,  _______,                             _______,                            _______,  _______,  _______,  _______, _______, _______),
 
     [_FN3] = LAYOUT_ansi_52(
         KC_TILD,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,          KC_BSLS,          _______,
-        KC_CAPS, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,      _______,  _______, _______, _______, _______, _______,          TO(MAC_BASE),
+        KC_CAPS, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,      _______,  _______, _______, _______, _______, _______,          _______,
         _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12, _______, _______, _______,  _______,            _______, _______,
-        _______, _______,  _______,                             _______,                            _______,  TO(_FN1),  TO(MAC_BASE),  _______, _______, _______),
+        _______, _______,  _______,                             _______,                            _______,  _______,  _______,  _______, _______, _______),
 
     [WIN_BASE] = LAYOUT_ansi_52(
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_MUTE,
@@ -86,6 +86,6 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [_FN1]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
     [_FN2]   = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [_FN3]   = {ENCODER_CCW_CW(_______, _______)}
+    [_FN3]   = {ENCODER_CCW_CW(KC_DOWN, KC_UP)}
 };
 #endif // ENCODER_MAP_ENABLE
