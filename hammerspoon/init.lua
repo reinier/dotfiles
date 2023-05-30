aea2d4e2000bd8c78e3b7a16a1172a12c3e0bda8
Reinier Ladan <reinier@ladan.email>
2023-05-30 13:27:26 +0000

    Tried Yabai

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -11,9 +11,45 @@ mehkey = { "ctrl", "alt", "shift" }
 reimod = mehkey
 
 require('ergomouse')
--- require('hammerbrowser')
 require('showkeymap')
--- require('windowmanagement')
+-- require('leader')
+menuHammer = hs.loadSpoon("MenuHammer")
+menuHammer:enter()
+
 
 hsImage = hs.loadSpoon('FadeLogo')
 hsImage:start()
+
+-- ~/.hammerspoon/keyboard/yabai.lua
+-- Send message(s) to a running instance of yabai.
+local function yabai(commands)
+	for _, cmd in ipairs(commands) do
+		os.execute("/opt/homebrew/bin/yabai -m " .. cmd)
+	end
+end
+
+local function yabaimod(key, commands)
+	hs.hotkey.bind(mehkey, key, function()
+		yabai(commands)
+	end)
+end
+
+yabaimod("j", { "window --focus next" })
+yabaimod("l", { "window --focus prev" })
+yabaimod("u", { "display --focus 1" })
+yabaimod("i", { "display --focus 2" })
+yabaimod("o", { "display --focus 3" })
+
+
+-- alpha
+-- yabaimod("f", { "window --toggle zoom-fullscreen" })
+-- yabaimod("l", { "space --focus recent" })
+-- yabaimod("m", { "space --toggle mission-control" })
+-- yabaimod("p", { "window --toggle pip" })
+-- yabaimod("g", { "space --toggle padding", "space --toggle gap" })
+-- yabaimod("r", { "space --rotate 90" })
+-- yabaimod("t", { "window --toggle float", "window --grid 4:4:1:1:2:2" })
+
+yabaimod("y", { "config layout bsp" })
+yabaimod("z", { "config layout float" })
+-- yabaimod("tab", { "space --focus recent" })
