948b6daf20b6b1ba1aeeb65f80a79801ccc790a6
Reinier Ladan <6823+reinier@users.noreply.github.com>
2025-09-09 16:43:10 +0200

    Cleaned up init

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -16,16 +16,13 @@ require('showkeymap')
 hs.hotkey.bind(reimod, "=", toggleKeymap)
 hs.hotkey.bind('', "F18", toggleKeymap)
 
--- menuHammer = hs.loadSpoon("MenuHammer")
--- menuHammer:enter()
-
-Install:installSpoonFromZipURL('https://github.com/mattorb/MenuHammer/raw/master/Spoons/MenuHammer.spoon.zip')
+Install:installSpoonFromZipURL('https://github.com/reinier/MenuHammer/raw/master/Spoons/MenuHammer.spoon.zip')
 local menuHammer = hs.loadSpoon("MenuHammer")
 menuHammer:enter()
-
--- hsImage = hs.loadSpoon('FadeLogo')
--- hsImage:start()
 
 -- Initialize directory watchers
 local directoryWatchers = require('functions.directory-watchers')
 directoryWatchers.start()
+
+-- Show reload confirmation message
+hs.alert.show("✅ Hammerspoon loaded", 2)
